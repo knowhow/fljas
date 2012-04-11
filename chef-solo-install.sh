f370a0d106cdf7ec23a280c808b586b1413c8611
Ernad Husremovic <hernad@bring.out.ba>
2012-04-11 16:32:49 +0200

    ruby

--- chef-solo-install.sh
+++ chef-solo-install.sh
@@ -2,13 +2,15 @@
 
 # This runs as root on the server
 
-chef_binary=/var/lib/gems/1.9.1/bin/chef-solo
+chef_binary=/usr/local/bin/chef-solo
 
 # Are we on a vanilla system?
 if ! test -f "$chef_binary"; then
         # Install Ruby and Chef
         apt-get install -y ruby1.9.1 ruby1.9.1-dev make &&
         gem1.9.1 install --no-rdoc --no-ri chef --version 0.10.0
-fi &&
+else
+        echo ruby, chef vec instalirani 
+fi
 
 chef-solo --version
