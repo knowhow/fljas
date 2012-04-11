506b791df7013cadfbdba72c04cd7c92ec65d458
Ernad Husremovic <hernad@bring.out.ba>
2012-04-11 17:14:24 +0200

    clean before repackage vagrant

---
+++ scripts/clean_before_vagrant_package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get clean
+
+# izbriši ovo da se prilikom izmjene MAC adrese ne bi promjenio raspored eth uređaja 
+rm /etc/udev/rules.d/70-persistent-net.rules 
+
+#echo "radim dd dev zero radi optimizacije prostora na vagrant sesiji ..."
+#echo " "
+# Zero out the free space to save space in the final image:
+#dd if=/dev/zero of=/EMPTY bs=1M
+#rm -f /EMPTY
