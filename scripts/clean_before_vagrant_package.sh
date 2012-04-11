d5ee84ae791e5366cf5dea2c918601e1d24c6154
Ernad Husremovic <hernad@bring.out.ba>
2012-04-11 17:15:28 +0200

    +x

--- scripts/clean_before_vagrant_package.sh
+++ scripts/clean_before_vagrant_package.sh
@@ -7,6 +7,7 @@ rm /etc/udev/rules.d/70-persistent-net.rules
 
 #echo "radim dd dev zero radi optimizacije prostora na vagrant sesiji ..."
 #echo " "
+
 # Zero out the free space to save space in the final image:
 #dd if=/dev/zero of=/EMPTY bs=1M
 #rm -f /EMPTY
